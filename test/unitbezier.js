f5add10d06d6df186c85982520d889805da5c6fa
Tom MacWright <tom@macwright.org>
2014-06-23 17:30:11 -0400

    Bump test coverage

--- test/unitbezier.js
+++ test/unitbezier.js
@@ -5,5 +5,10 @@ test('unit bezier', function(t) {
     var u = new UnitBezier(0, 0, 1, 1);
     t.equal(u.sampleCurveY(1), 1, 'sampleCurveY');
     t.equal(u.sampleCurveX(1), 1, 'sampleCurveX');
+    t.equal(u.sampleCurveDerivativeX(0.1), 0.54, 'sampleCurveDerivativeX');
+    t.equal(u.solveCurveX(0), 0, 'solveCurveX');
+    t.equal(u.solveCurveX(1), 1, 'solveCurveX');
+    t.equal(u.solveCurveX(0.5), 0.5, 'solveCurveX');
+    t.equal(u.solve(0.5), 0.5, 'solve');
     t.end();
 });
