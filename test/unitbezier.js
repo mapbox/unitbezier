c3e0d07b20d3219a99367d676f75f66021358838
Tom MacWright <tom@macwright.org>
2017-01-17 16:14:25 -0500

    test(unit): Improve test coverage

--- test/unitbezier.js
+++ test/unitbezier.js
@@ -8,6 +8,8 @@ test('unit bezier', function(t) {
     t.equal(u.sampleCurveDerivativeX(0.1), 0.54, 'sampleCurveDerivativeX');
     t.equal(u.solveCurveX(0), 0, 'solveCurveX');
     t.equal(u.solveCurveX(1), 1, 'solveCurveX');
+    t.equal(u.solveCurveX(1.25552, 1.e-8), 1, 'solveCurveX');
+    t.equal(u.solveCurveX(1, 1e-8), 1, 'solveCurveX');
     t.equal(u.solveCurveX(0.5), 0.5, 'solveCurveX');
     t.equal(u.solve(0.5), 0.5, 'solve');
     t.end();
