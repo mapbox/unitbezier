7db43a814f4b9cd30d47a4f0679946ce8ca1bfa1
Vladimir Agafonkin <agafonkin@gmail.com>
2017-09-08 17:08:32 +0300

    Various cleanup (#1)
    * cut out license into separate file
    
    * shorter method definitions
    
    * clean up documentation
    
    * clean up code a bit
    
    * add eslint
    
    * bring back p1x/etc props
    
    * remove commitizen stuff
    
    UnitBezier is rarely updated so no need for additional maintenance tools

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-/*
- * Copyright (C) 2008 Apple Inc. All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Ported from Webkit
- * http://svn.webkit.org/repository/webkit/trunk/Source/WebCore/platform/graphics/UnitBezier.h
- */
+'use strict';
 
 module.exports = UnitBezier;
 
@@ -39,67 +13,66 @@ function UnitBezier(p1x, p1y, p2x, p2y) {
     this.ay = 1.0 - this.cy - this.by;
 
     this.p1x = p1x;
-    this.p1y = p2y;
+    this.p1y = p1y;
     this.p2x = p2x;
     this.p2y = p2y;
 }
 
-UnitBezier.prototype.sampleCurveX = function(t) {
-    // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
-    return ((this.ax * t + this.bx) * t + this.cx) * t;
-};
+UnitBezier.prototype = {
+    sampleCurveX: function (t) {
+        // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
+        return ((this.ax * t + this.bx) * t + this.cx) * t;
+    },
 
-UnitBezier.prototype.sampleCurveY = function(t) {
-    return ((this.ay * t + this.by) * t + this.cy) * t;
-};
+    sampleCurveY: function (t) {
+        return ((this.ay * t + this.by) * t + this.cy) * t;
+    },
 
-UnitBezier.prototype.sampleCurveDerivativeX = function(t) {
-    return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
-};
+    sampleCurveDerivativeX: function (t) {
+        return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
+    },
 
-UnitBezier.prototype.solveCurveX = function(x, epsilon) {
-    if (typeof epsilon === 'undefined') epsilon = 1e-6;
+    solveCurveX: function (x, epsilon) {
+        if (epsilon === undefined) epsilon = 1e-6;
 
-    var t0, t1, t2, x2, i;
+        if (x < 0.0) return 0.0;
+        if (x > 1.0) return 1.0;
 
-    // First try a few iterations of Newton's method -- normally very fast.
-    for (t2 = x, i = 0; i < 8; i++) {
+        var t = x;
 
-        x2 = this.sampleCurveX(t2) - x;
-        if (Math.abs(x2) < epsilon) return t2;
+        // First try a few iterations of Newton's method - normally very fast.
+        for (var i = 0; i < 8; i++) {
+            var x2 = this.sampleCurveX(t) - x;
+            if (Math.abs(x2) < epsilon) return t;
 
-        var d2 = this.sampleCurveDerivativeX(t2);
-        if (Math.abs(d2) < 1e-6) break;
+            var d2 = this.sampleCurveDerivativeX(t);
+            if (Math.abs(d2) < 1e-6) break;
 
-        t2 = t2 - x2 / d2;
-    }
-
-    // Fall back to the bisection method for reliability.
-    t0 = 0.0;
-    t1 = 1.0;
-    t2 = x;
-
-    if (t2 < t0) return t0;
-    if (t2 > t1) return t1;
-
-    while (t0 < t1) {
-
-        x2 = this.sampleCurveX(t2);
-        if (Math.abs(x2 - x) < epsilon) return t2;
-
-        if (x > x2) {
-            t0 = t2;
-        } else {
-            t1 = t2;
+            t = t - x2 / d2;
         }
 
-        t2 = (t1 - t0) * 0.5 + t0;
+        // Fall back to the bisection method for reliability.
+        var t0 = 0.0;
+        var t1 = 1.0;
+        t = x;
+
+        for (i = 0; i < 20; i++) {
+            x2 = this.sampleCurveX(t);
+            if (Math.abs(x2 - x) < epsilon) break;
+
+            if (x > x2) {
+                t0 = t;
+            } else {
+                t1 = t;
+            }
+
+            t = (t1 - t0) * 0.5 + t0;
+        }
+
+        return t;
+    },
+
+    solve: function (x, epsilon) {
+        return this.sampleCurveY(this.solveCurveX(x, epsilon));
     }
-
-    // Failure.
-    return t2;
-};
-
-UnitBezier.prototype.solve = function(x, epsilon) {
-    return this.sampleCurveY(this.solveCurveX(x, epsilon));
 };
