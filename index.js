90aa25b5b6bb92b3a92cd2c716f4d2f8633bd9c8
Vladimir Agafonkin <agafonkin@gmail.com>
2017-09-08 13:17:36 +0300

    shorter method definitions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 
 module.exports = UnitBezier;
 
@@ -10,69 +11,66 @@ function UnitBezier(p1x, p1y, p2x, p2y) {
     this.cy = 3.0 * p1y;
     this.by = 3.0 * (p2y - p1y) - this.cy;
     this.ay = 1.0 - this.cy - this.by;
-
-    this.p1x = p1x;
-    this.p1y = p2y;
-    this.p2x = p2x;
-    this.p2y = p2y;
 }
 
-UnitBezier.prototype.sampleCurveX = function(t) {
-    // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
-    return ((this.ax * t + this.bx) * t + this.cx) * t;
-};
+UnitBezier.prototype = {
+    sampleCurveX: function(t) {
+        // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
+        return ((this.ax * t + this.bx) * t + this.cx) * t;
+    },
 
-UnitBezier.prototype.sampleCurveY = function(t) {
-    return ((this.ay * t + this.by) * t + this.cy) * t;
-};
+    sampleCurveY: function(t) {
+        return ((this.ay * t + this.by) * t + this.cy) * t;
+    },
 
-UnitBezier.prototype.sampleCurveDerivativeX = function(t) {
-    return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
-};
+    sampleCurveDerivativeX: function(t) {
+        return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
+    },
 
-UnitBezier.prototype.solveCurveX = function(x, epsilon) {
-    if (typeof epsilon === 'undefined') epsilon = 1e-6;
+    solveCurveX: function(x, epsilon) {
+        if (epsilon === undefined) epsilon = 1e-6;
 
-    var t0, t1, t2, x2, i;
+        var t0, t1, t2, x2, i;
 
-    // First try a few iterations of Newton's method -- normally very fast.
-    for (t2 = x, i = 0; i < 8; i++) {
+        // First try a few iterations of Newton's method -- normally very fast.
+        for (t2 = x, i = 0; i < 8; i++) {
 
-        x2 = this.sampleCurveX(t2) - x;
-        if (Math.abs(x2) < epsilon) return t2;
+            x2 = this.sampleCurveX(t2) - x;
+            if (Math.abs(x2) < epsilon) return t2;
 
-        var d2 = this.sampleCurveDerivativeX(t2);
-        if (Math.abs(d2) < 1e-6) break;
+            var d2 = this.sampleCurveDerivativeX(t2);
+            if (Math.abs(d2) < 1e-6) break;
 
-        t2 = t2 - x2 / d2;
-    }
-
-    // Fall back to the bisection method for reliability.
-    t0 = 0.0;
-    t1 = 1.0;
-    t2 = x;
-
-    if (t2 < t0) return t0;
-    if (t2 > t1) return t1;
-
-    while (t0 < t1) {
-
-        x2 = this.sampleCurveX(t2);
-        if (Math.abs(x2 - x) < epsilon) return t2;
-
-        if (x > x2) {
-            t0 = t2;
-        } else {
-            t1 = t2;
+            t2 = t2 - x2 / d2;
         }
 
-        t2 = (t1 - t0) * 0.5 + t0;
+        // Fall back to the bisection method for reliability.
+        t0 = 0.0;
+        t1 = 1.0;
+        t2 = x;
+
+        if (t2 < t0) return t0;
+        if (t2 > t1) return t1;
+
+        while (t0 < t1) {
+
+            x2 = this.sampleCurveX(t2);
+            if (Math.abs(x2 - x) < epsilon) return t2;
+
+            if (x > x2) {
+                t0 = t2;
+            } else {
+                t1 = t2;
+            }
+
+            t2 = (t1 - t0) * 0.5 + t0;
+        }
+
+        // Failure.
+        return t2;
+    },
+
+    solve: function(x, epsilon) {
+        return this.sampleCurveY(this.solveCurveX(x, epsilon));
     }
-
-    // Failure.
-    return t2;
-};
-
-UnitBezier.prototype.solve = function(x, epsilon) {
-    return this.sampleCurveY(this.solveCurveX(x, epsilon));
 };
